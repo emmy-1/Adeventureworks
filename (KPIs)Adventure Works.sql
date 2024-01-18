e4342b99213dab4b4ea3ca9e69aa12afaffc639d
emmy-1 <50409804+emmy-1@users.noreply.github.com>
2024-01-18 16:29:19 +0000

    Update to KPIS Adventure Work
    Working On creating the total sales by category.

--- (KPIs)Adventure Works.sql
+++ (KPIs)Adventure Works.sql
@@ -15,3 +15,36 @@ EXEC TotalSalesperyear @OrderDate = 2014
 Select YEAR (OrderDate) as Year, Sum(TotalDue) AS TotalSales
 from Sales.SalesOrderHeader
 GROUP BY YEAR (OrderDate)
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* View Created to join the Production.productsubcategory and Production.ProductCategory.  Uncheck to run view*/
+/*CREATE VIEW ProductDetails AS*/ 
+Select pd.ProductCategoryID, pc.ProductSubcategoryID, pd.Name , Pc.Name as ProductSubName
+from Production.ProductCategory as pd
+inner join Production.ProductSubcategory as pc
+ON pd.ProductCategoryID = pc.ProductCategoryID
+GROUP BY pd.ProductCategoryID, pc.ProductSubcategoryID, pd.Name , Pc.Name 
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+Create View ProductInfo
+Select p.ProductID, p.ProductSubcategoryID, p.Name ,sd.OrderQty,sd.LineTotal
+from Sales.SalesOrderDetail as sd
+JOIN Production.Product as p
+ON sd.ProductID = p.ProductID 
+GROUP BY p.ProductID, p.ProductSubcategoryID, p.Name ,sd.OrderQty,sd.LineTotal
+
+
+
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+select * from Sales.SalesOrderHeader
+select * from Sales.SalesOrderDetail
+select * from Production.Product
+select * from Production.ProductCategory
+select * from Production.ProductSubcategory
+
+Select 
